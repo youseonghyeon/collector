56a0b7777fc14497ff893dd7434efa5d1cbbdbc6
youseonghyeon <dolla_@naver.com>
2021-12-23 18:08:41 +0900

    Initial commit

---
+++ src/main/java/study/collector/entity/Member.java
@@ -0,0 +1,17 @@
+package study.collector.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+}
